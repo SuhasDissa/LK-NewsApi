0a864b1b15f438a8da2be1548afd870a0efea89e
Suhas Dissanayake <suhasdissa@protonmail.com>
2022-12-21 16:50:07 +0530

    Add Adaderana scraper

--- sources/adaderana.py
+++ sources/adaderana.py
@@ -6,9 +6,9 @@ from bs4 import BeautifulSoup
 from helper.html_scraper import Scraper
 
 
-class GossipLanka:
+class AdaDerana:
     def __init__(self):
-        self.BASE_URL = "https://www.gossiplankanews.com/"
+        self.BASE_URL = "https://sinhala.adaderana.lk/sinhala-hot-news.php"
 
     def _parser_topnews(self, htmls):
         try:
@@ -16,13 +16,13 @@ class GossipLanka:
                 soup = BeautifulSoup(html, "lxml")
                 my_dict = {"data": []}
 
-                for div in soup.find(id="main") \
-                        .find("div", class_="grid-posts") \
-                        .find_all("div", class_="blog-post"):
+                for div in soup.find("main", class_="main-content") \
+                        .find("div", class_="container") \
+                        .find_all("div", class_="news-story"):
                     url = div.find("h2").a["href"]
                     title = div.find("h2").a.text
-                    image_link = div.find("span", class_="post-thumb")["image-src"]
-                    snippet = div.find("div", class_="snippet-item").text
+                    image_link = div.find("img")["src"]
+                    snippet = div.find("p").text
                     encoded_url = quote(url)
                     my_dict["data"].append({"url": url})
                     my_dict["data"].append({"title": title})
@@ -39,11 +39,12 @@ class GossipLanka:
                 soup = BeautifulSoup(html, "lxml")
                 my_dict = {"post": []}
 
-                div = soup.find(id="main") \
-                    .find("div", class_="post-item-inner")
+                div = soup.find("main", class_="main-content") \
+                    .find("div", class_="container") \
+                    .find("article", class_="news")
 
                 title = div.find("h1").text
-                body = div.find("div", class_="post-body").text
+                body = div.find("div", class_="news-content").text
 
                 my_dict["post"].append({"title": title})
                 my_dict["post"].append({"body": body})
