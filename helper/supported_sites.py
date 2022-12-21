956586675ae875cebd1029b3a818cf0df85ebb85
Suhas Dissanayake <suhasdissa@protonmail.com>
2022-12-21 11:25:01 +0000

    add adaderana to supported sites

--- helper/supported_sites.py
+++ helper/supported_sites.py
@@ -1,7 +1,11 @@
 from sources.gossiplanka import GossipLanka
+from sources.adaderana import AdaDerana
 
 supported_sites = {
     "gossiplanka": {
         "website": GossipLanka
+    },
+    "adaderana":{
+        "website": AdaDerana
     }
 }
